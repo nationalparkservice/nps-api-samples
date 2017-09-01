6390d87d32f0e4f361fcce6045672ae3d480900a
Todd M. Edgar <tmedgar@users.noreply.github.com>
2017-09-01 13:14:33 -0400

    Create park-lat-long.php

---
+++ park-lat-long.php
@@ -0,0 +1,21 @@
+<?php
+	// Get cURL resource
+	$curl = curl_init();
+	// Set options
+	$park = 'acad';
+	$dataURL = 'https://developer.nps.gov/api/v0/parks?parkCode=' . $park;
+	curl_setopt_array($curl, array(
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_URL => $dataURL,
+		CURLOPT_USERAGENT => $_SERVER['HTTP_USER_AGENT'],
+		CURLOPT_HTTPHEADER => array('Authorization: INSERT-API-KEY-HERE')
+	));
+	// Send the request & save response to $response
+	$response = curl_exec($curl);
+	// Close request to clear up some resources
+	curl_close($curl);
+	$json = json_decode($response);
+	
+	// Prepare and execute address output
+	echo $json->data[0]->fullName, " can be found at ", $json->data[0]->latLong, ".";
+?>
